383ed16307c10335585896cba1cdf56c38cb4665
Lendar <xeldim@gmail.com>
2015-09-02 16:11:32 +0300

    Bump version to 16.5.4

--- bin/Tools/modules/variables.js
+++ bin/Tools/modules/variables.js
@@ -1,6 +1,6 @@
-var version = "16.5.0";
+var version = "16.5.4";
 var verType = "";
-var buildDate = "2015/08/12"; // YYYY/MM/DD
+var buildDate = "2015/09/02"; // YYYY/MM/DD
 var Reg = "HKCU\\SOFTWARE\\drpsu\\";
 
 var WshShell = new ActiveXObject("WScript.Shell");
