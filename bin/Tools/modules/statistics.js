c12864b6151948aee94fd82765b8c3b2fe2e9ae3
Lendar <xeldim@gmail.com>
2015-09-18 13:16:49 +0300

    Read and write ClientID from Registry

--- bin/Tools/modules/statistics.js
+++ bin/Tools/modules/statistics.js
@@ -1,3 +1,109 @@
+(function () {
+  var log = function log() {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return window.log("[clientIdStorage] " + args.join(' '));
+  };
+  var Reg = 'HKCU\\SOFTWARE\\drpsu\\';
+
+  var WshShell = new ActiveXObject('WScript.Shell');
+  var fso = new ActiveXObject('Scripting.FileSystemObject');
+
+  var RegRead32 = function RegRead32(key) {
+    var ret = '';
+    try {
+      ret = WshShell.RegRead(key);
+    } catch (e) {
+      ret = '';
+    }
+    return ret;
+  };
+
+  var RegRead = function RegRead(key) {
+    key = key.replace('HKEY_LOCAL_MACHINE\\', 'HKLM\\');
+    key = key.replace('HKEY_CURRENT_USER\\', 'HKCU\\');
+    var ret = RegRead32(key);
+    if (!ret && key.indexOf('\\SOFTWARE\\Microsoft\\') !== -1) {
+      key = key.replace('\\SOFTWARE\\Microsoft\\', '\\SOFTWARE\\Wow6432Node\\Microsoft\\');
+      ret = RegRead32(key);
+    }
+    return ret;
+  };
+
+  var RegWrite = function RegWrite(key, value) {
+    WshShell.RegWrite(key, value, 'REG_SZ');
+  };
+
+  var clientIdStorage = {
+    clientIdKey: Reg + 'ClientID',
+
+    init: function init() {
+      if (this.clientId) {
+        return this.clientId;
+      }
+      this.clientId = this.readClientIdFromFile();
+      if (this.clientId) {
+        log("got clientId from file " + this.clientId);
+        this.saveClientIdToRegistry(this.clientId);
+        return this.clientId;
+      }
+      this.clientId = this.readClientIdFromRegistry();
+      if (this.clientId) {
+        log("got clientId from registry " + this.clientId);
+        return this.clientId;
+      }
+      this.clientId = this.generate();
+      log("generated clientId " + this.clientId);
+      this.saveClientIdToRegistry(this.clientId);
+      return this.clientId;
+    },
+
+    generate: function generate() {
+      /* eslint-disable */
+      var d = new Date().getTime();
+      var uuid = 'xxxxxxxxx.xxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 10) % 10 | 0;
+        return (c === 'x' ? r : r & 0x7 | 0x8).toString();
+      });
+      return uuid;
+      /* eslint-enable */
+    },
+
+    readClientIdFromRegistry: function readClientIdFromRegistry() {
+      return RegRead(this.clientIdKey);
+    },
+
+    saveClientIdToRegistry: function saveClientIdToRegistry(clientId) {
+      RegWrite(this.clientIdKey, clientId);
+    },
+
+    parseClientIdJs: function parseClientIdJs(text) {
+      text = text.substr(text.indexOf("'") + 1, text.indexOf("'", text.indexOf("'") + 1) - text.indexOf("'") - 1);
+      var pos = text.indexOf("_");
+      var lenght = text.indexOf(".exe");
+      var id = text.substr(pos + 1, lenght - pos - 1);
+      this.clientId = id;
+    },
+
+    readClientIdFromFile: function readClientIdFromFile() {
+      var file = 'tools\\modules\\clientid.js';
+      if (fso.FileExists(file)) {
+        var text = fso.GetFile(file);
+        if (text.Size > 0) {
+          var contents = text.OpenAsTextStream(1).ReadAll();
+          var clientId = this.parseClientIdJs(contents);
+          return clientId;
+        }
+      }
+    }
+  };
+
+  window.clientIdStorage = clientIdStorage;
+})();
+
+
 var statistics = {
     //clientId: "000000000.0000000000",
     clientId: "",
@@ -46,15 +152,7 @@ var statistics = {
     init: function () {
 		this.initYaMetrika();
 
-        var file = "tools\\modules\\clientid.js";
-        if (fso.FileExists(file)) {
-            var text = fso.GetFile(file);
-            if (text.Size > 0) {
-                this.setClientId(text.OpenAsTextStream(1).ReadAll());
-            }
-        } else {
-            this.clientId = this.generate();
-        }
+        this.clientId = clientIdStorage.init()
     },
 	initYaMetrika: function(){
 		(function (d, w, c) {
