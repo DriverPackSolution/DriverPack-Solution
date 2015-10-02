db21de3862362d5dc39ee0ed3ebafeda47d1ee6e
Lendar <xeldim@gmail.com>
2015-10-02 14:43:28 +0300

    gulp: Automate push of update repo on release

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,13 +8,16 @@ import gutil from 'gulp-util'
 Promise.promisifyAll(fs)
 Promise.promisifyAll(git)
 
-const deployDir = 'update.drp.su'
-const cwd = path.join(__dirname, deployDir)
+const REPO_URL = 'git@github.com:DriverPackSolution/update.drp.su.git'
+const DEPLOY_DIR = 'update.drp.su'
+const REMOTE = 'origin'
+
+const cwd = path.join(__dirname, DEPLOY_DIR)
 
 function copyDist (version) {
   return new Promise((resolve, reject) => {
     gulp.src('./bin/**/*.{html,hta,png,jpg,gif,ico,ttf,css,js}', {base: './bin'})
-      .pipe(gulp.dest(path.join('.', deployDir, 'beetle', version)))
+      .pipe(gulp.dest(path.join('.', DEPLOY_DIR, 'beetle', version)))
       .on('end', resolve)
       .on('error', reject)
   })
@@ -38,23 +41,48 @@ function getVersion () {
   })
 }
 
-gulp.task('release', () => {
-  let release
-  return getVersion()
-    .then(version => {
-      release = {
-        version,
-        commitMessage: `Release beetle v${version}`
-      }
+const updateProject = {
+  setActiveVersion (nextVersion) {
+    const scriptFile = path.join(DEPLOY_DIR, 'v2/index.html')
+    return fs.readFileAsync(scriptFile)
+    .then(contents => {
+      const patchedContents = contents.toString()
+        .replace(
+          /http:\/\/[^\.]+.drp.su\/beetle\/([^\\]+)\//,
+          (match, currentVersion) => {
+            gutil.log(`Patching ${scriptFile}: ${currentVersion} => ${nextVersion}`)
+            return match.replace(currentVersion, nextVersion)
+          }
+        )
+      return fs.writeFileAsync(scriptFile, patchedContents)
     })
-    .then(() => gutil.log(release.commitMessage))
-    .then(() => {
-      return git
-        .statusAsync({cwd, quiet: true})
-        .catch(() => git.cloneAsync('git@bitbucket.org:artxgroup/update.drp.su.git'))
-    })
-    .then(() => git.checkoutAsync('master', {cwd}))
-    .then(() => copyDist(release.version))
-    .then(() => commitFiles(release.commitMessage))
-})
+  }
+}
 
+async function release (branch = 'master') {
+  let release
+  const version = await getVersion()
+  release = {
+    version,
+    commitMessage: `Release beetle v${version}`
+  }
+  gutil.log(release.commitMessage)
+  try {
+    await git.statusAsync({cwd, quiet: true})
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err
+    }
+    gutil.log(`Cloning ${REPO_URL} to ${DEPLOY_DIR}`)
+    await git.cloneAsync(REPO_URL, {args: DEPLOY_DIR})
+  }
+  await git.checkoutAsync(branch, {cwd})
+  await git.pullAsync(REMOTE, branch, {cwd, args: '--rebase'})
+  await copyDist(release.version)
+  await updateProject.setActiveVersion(version)
+  await commitFiles(release.commitMessage)
+  await git.pushAsync(REMOTE, branch, {cwd})
+}
+
+gulp.task('release:prod', () => release('master'))
+gulp.task('release:test', () => release('dev'))
