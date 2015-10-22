9e018ec9d4b9608752e2f31572d98214a37396f0
Lendar <xeldim@gmail.com>
2015-10-22 19:16:32 +0300

    16.7.2

--- bin/Tools/modules/variables.js
+++ bin/Tools/modules/variables.js
@@ -1,6 +1,6 @@
-var version = "16.7.0";
+var version = "16.7.2";
 var verType = "";
-var buildDate = "2015/10/02"; // YYYY/MM/DD
+var buildDate = "2015/10/22"; // YYYY/MM/DD
 var Reg = "HKCU\\SOFTWARE\\drpsu\\";
 
 var WshShell = new ActiveXObject("WScript.Shell");
